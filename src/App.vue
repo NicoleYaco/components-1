e8a1f6b64002435368a792b4bf2a03e7654ef975
Nicole Yaco <yacorinicole1003@gmail.com>
2024-08-31 10:14:11 -0500

    subiendo archivos

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import BlogPost from './components/BlogPost.vue';
+import ButtonCounter from './components/ButtonCounter.vue';
+import { ref } from 'vue';
+
+const posts = ref([
+  { title: 'Post 1', id: 1, body: 'descripción 1' },
+  { title: 'Post 2', id: 2, body: 'descripción 2' },
+  { title: 'Post 3', id: 3, body: 'descripción 3' },
+]);
+
+const favorito = ref("");
+const cambiarFavorito = (title) => {
+  favorito.value = title;
+};
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>APP</h1>
+    <h2>Mi Post Favorito: {{ favorito }}</h2>
+    <ButtonCounter />
+    <BlogPost 
+    v-for="post in posts"
+    :key="post.id" 
+    :title="post.title" 
+    :id="post.id" 
+    body="post.body" @cambiarFavorito="cambiarFavorito"
+    ></BlogPost>
+
+  </div>
+</template>
---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue';
+const counter = ref (0)
+const increment = (first) => counter.value ++
+</script>
+
+<template>
+    <button @click="increment">Counter{{ counter }}</button>
+</template>
+
+<style>
+</style>
